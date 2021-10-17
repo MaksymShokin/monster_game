fac24e93c172eda8118d46da947e58bd4cdb53d4
Maksym Shokin <problaze7@gmail.com>
2021-10-17 10:39:17 +0300

    start-game

---
+++ interactions/interactions.go
@@ -0,0 +1,9 @@
+package interactions
+
+import "fmt"
+
+func PrintGreetings() {
+	fmt.Println("MONSTER SLAYER")
+	fmt.Println("moster is comming...")
+	fmt.Println("GOOD LUCK")
+}
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import "monster_game/interactions"
+
 func main() {
-	
+	startGame()
+
+	winner := ""
+
+	for winner == "" {
+		winner = executeRound()
+	}
+
+	endGame()
+}
+
+func startGame() {
+	interactions.PrintGreetings()
+
+}
+func executeRound() string {
+	return ""
+}
+
+func endGame() {
+
 }
